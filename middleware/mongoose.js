d8d77256d0caf6802d9de10c5c78601d5ddf25bf
Nilesh Deore <nileshdeore2623@gmail.com>
2024-06-14 21:43:12 +0530

    Update mongoose.js

--- middleware/mongoose.js
+++ middleware/mongoose.js
@@ -4,9 +4,9 @@ const connectDb = handler => async (req, res)=>{
     if(mongoose.connections[0].readyState){
         return handler (req, res)
     }
-    await mongoose.connect(process.env.MONGO_URI)
+    // await mongoose.connect(process.env.MONGO_URI)
     // await mongoose.connect("mongodb://localhost:27017/next-atlus")
-    // await mongoose.connect("mongodb+srv://nileshdeore:12501250@cluster0.qogyo0p.mongodb.net/next-atlus?retryWrites=true&w=majority&appName=Cluster0")
+    await mongoose.connect("mongodb+srv://nileshdeore:12501250@cluster0.qogyo0p.mongodb.net/next-atlus?retryWrites=true&w=majority&appName=Cluster0")
     
     return handler(req, res);
 }
